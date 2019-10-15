96b35e0436de6377e780f9cebd6a595582e06a45
amosbastian <amos_bastian@hotmail.com>
2019-10-16 00:22:48 +0100

    Add default about & projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ProjectsPage = () => (
+  <Layout>
+    <SEO title="Projects" />
+    <h1>Projects</h1>
+  </Layout>
+)
+
+export default ProjectsPage
